83ac729023df4761bfb85d99c4209f13bf04f547
김명준 <86913355+mjj111@users.noreply.github.com>
2024-10-07 19:53:32 +0900

    Title: 메이즈 러너, Time: 247ms, Memory: 10MB, Status: Passed - CodeTree

--- 241007/메이즈 러너/maze-runner.java
+++ 241007/메이즈 러너/maze-runner.java
@@ -1,209 +1,179 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
+
+class Pair {
+    int x, y;
+
+    public Pair(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+};
 
 public class Main {
+    public static final int MAX_N = 10;
     
-    private static int[][] board;
-    private static int[] exit;
-    private static int[] dx = {-1, 1, 0, 0};
-    private static int[] dy = {0, 0, 1, -1};
-
-    private static int N, M;
-    private static Person[] people;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-
-        board = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++) {
-                board[i][j] = Integer.parseInt(st.nextToken());
+    public static int n, m, k;
+    
+    public static int[][] board = new int[MAX_N + 1][MAX_N + 1];
+    
+    public static int[][] nextBoard = new int[MAX_N + 1][MAX_N + 1];
+    
+    public static Pair[] traveler = new Pair[MAX_N + 1];
+    
+    public static Pair exits;
+    
+    public static int ans;
+    
+    public static int sx, sy, squareSize;
+    
+    public static void moveAllTraveler() {
+        for(int i = 1; i <= m; i++) {
+            if(traveler[i].x == exits.x && traveler[i].y == exits.y)
+                continue;
+            
+            if(traveler[i].x != exits.x) {
+                int nx = traveler[i].x;
+                int ny = traveler[i].y;
+    
+                if(exits.x > nx) nx++;
+                else nx--;
+    
+                if(board[nx][ny] == 0) {
+                    traveler[i].x = nx;
+                    traveler[i].y = ny;
+                    ans++;
+                    continue;
+                }
             }
-        }
-
-        people = new Person[M];
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            people[i] = new Person(x - 1, y - 1);
-        }
-
-        exit = new int[2];
-        st = new StringTokenizer(br.readLine());
-        exit[0] = Integer.parseInt(st.nextToken()) - 1;
-        exit[1] = Integer.parseInt(st.nextToken()) - 1;
-        board[exit[0]][exit[1]] = -2;
-
-        int time = 1;
-        while (K-- > 0 && keep()) {
-            peopleMove();
-            boardMove();
-            time++;
-        }
-
-        int count = 0;
-        for (Person p : people) {
-            count += p.moveCount;
-        }
-
-        System.out.println(count);
-        System.out.println((exit[0] + 1) + " " + (exit[1] + 1));
-    }
-
-    private static void peopleMove() {
-        for (Person now : people) {
-            if (!now.isIn) continue;
-            now.move();
-        }
-    }
-
-    private static void boardMove() {
-        Arrays.sort(people);
-        Person closest = people[0];
-
-        int minX = Math.min(exit[0], closest.x);
-        int maxX = Math.max(exit[0], closest.x);
-        int minY = Math.min(exit[1], closest.y);
-        int maxY = Math.max(exit[1], closest.y);
-
-        int squareSize = Math.max(maxX - minX, maxY - minY);
-
-        int startX = 0, endX = 0, startY = 0, endY = 0;
-        boolean found = false;
-        
-        for (int i = 0; i <= N - squareSize; i++) {
-            if (found) break;
-            for (int j = 0; j <= N - squareSize; j++) {
-                if (!isInRange(i, j, squareSize, closest, exit)) continue;
-                startX = i;
-                endX = i + squareSize;
-                startY = j;
-                endY = j + squareSize;
-                found = true;
-                break;
-            }
-        }
-
-        for (int i = startX; i <= endX; i++) {
-            for (int j = startY; j <= endY; j++) {
-                if (board[i][j] >= 1) board[i][j] -= 1;
-            }
-        }
-        
-        rotateBoard(startX, endX, startY, endY, squareSize);
-    }
-
-    private static void rotateBoard(int startX, int endX, int startY, int endY, int squareSize) {
-        int[][] copyBoard = new int[squareSize + 1][squareSize + 1];
-        
-        for (int i = 0; i <= squareSize; i++) {
-            for (int j = 0; j <= squareSize; j++) {
-                copyBoard[j][squareSize - i] = board[startX + i][startY + j];
-            }
-        }
-
-        for (int i = 0; i <= squareSize; i++) {
-            for (int j = 0; j <= squareSize; j++) {
-
-                board[startX + i][startY + j] = copyBoard[i][j];
-
-                if (board[startX + i][startY + j] == -2) {
-                    exit[0] = startX + i;
-                    exit[1] = startY + j;
+    
+            if(traveler[i].y != exits.y) {
+                int nx = traveler[i].x;
+                int ny = traveler[i].y;
+    
+                if(exits.y > ny) ny++;
+                else ny--;
+    
+                if(board[nx][ny] == 0) {
+                    traveler[i].x = nx;
+                    traveler[i].y = ny;
+                    ans++;
+                    continue;
                 }
             }
         }
-
-        for (Person person : people) {
-            if (person.x >= startX && person.x <= endX && person.y >= startY && person.y <= endY && person.isIn) {
-                int relativeX = person.x - startX;
-                int relativeY = person.y - startY;
-
-                int newX = startX + relativeY;
-                int newY = startY + (squareSize - relativeX);
-
-                person.x = newX;
-                person.y = newY;
-            }
-        }
     }
-
-    private static boolean keep() {
-        for (Person p : people) {
-            if (p.isIn) return true;
-        }
-        return false;
-    }
-
-    private static int getDistance(int x, int y) {
-        return Math.abs(exit[0] - x) + Math.abs(exit[1] - y);
-    }
-
-    private static boolean isOut(int x, int y) {
-        return x < 0 || x >= N || y < 0 || y >= N;
-    }
-
-    private static boolean isInRange(int startX, int startY, int size, Person closest, int[] exit) {
-        return closest.x >= startX && closest.x <= startX + size && closest.y >= startY && closest.y <= startY + size &&
-               exit[0] >= startX && exit[0] <= startX + size && exit[1] >= startY && exit[1] <= startY + size;
-    }
-
-    private static class Person implements Comparable<Person> {
-        int x, y;
-        int moveCount = 0;
-        boolean isIn = true;
-
-        public Person(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public void move() {
-            int nowDistance = getDistance(x, y);
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (isOut(nx, ny)) continue;
-                if (board[nx][ny] >= 1) continue;
-                if (getDistance(nx, ny) >= nowDistance) continue;
-
-                this.x = nx;
-                this.y = ny;
-                moveCount += 1;
-                if (exit[0] == x && exit[1] == y) isIn = false;
-                return;
-            }
-        }
-
-        @Override
-        public int compareTo(Person op) {
-            if (!this.isIn && op.isIn) {
-                return 1;
-            }
-            if (this.isIn && !op.isIn) {
-                return -1;
-            }
-            if (!this.isIn && !op.isIn) {
-                return 0;
-            }
-
-            int thisDistance = getDistance(this.x, this.y);
-            int opDistance = getDistance(op.x, op.y);
-
-            if (thisDistance == opDistance) {
-                if (this.x == op.x) {
-                    return this.y - op.y;
+    
+    public static void findMinimumSquare() {
+        for(int sz = 2; sz <= n; sz++) {
+            for(int x1 = 1; x1 <= n - sz + 1; x1++) {
+                for(int y1 = 1; y1 <= n - sz + 1; y1++) {
+                    int x2 = x1 + sz - 1;
+                    int y2 = y1 + sz - 1;
+    
+                    if(!(x1 <= exits.x && exits.x <= x2 && y1 <= exits.y && exits.y <= y2)) {
+                        continue;
+                    }
+    
+                    boolean isTravelerIn = false;
+                    for(int l = 1; l <= m; l++) {
+                        if(x1 <= traveler[l].x && traveler[l].x <= x2 && y1 <= traveler[l].y && traveler[l].y <= y2) {
+                            if(!(traveler[l].x == exits.x && traveler[l].y == exits.y)) {
+                                isTravelerIn = true;
+                            }
+                        }
+                    }
+    
+                    if(isTravelerIn) {
+                        sx = x1;
+                        sy = y1;
+                        squareSize = sz;
+    
+                        return;
+                    }
                 }
-                return this.x - op.x;
             }
-            return thisDistance - opDistance;
         }
+    }
+    
+    public static void rotateSquare() {
+        for(int x = sx; x < sx + squareSize; x++)
+            for(int y = sy; y < sy + squareSize; y++) {
+                if(board[x][y] > 0) board[x][y]--;
+            }
+    
+        for(int x = sx; x < sx + squareSize; x++)
+            for(int y = sy; y < sy + squareSize; y++) {
+                int ox = x - sx, oy = y - sy;
+                int rx = oy, ry = squareSize - ox - 1;
+                nextBoard[rx + sx][ry + sy] = board[x][y];
+            }
+    
+        for(int x = sx; x < sx + squareSize; x++)
+            for(int y = sy; y < sy + squareSize; y++) {
+                board[x][y] = nextBoard[x][y];
+            }
+    }
+    
+    public static void rotateTravelerAndExit() {
+        for(int i = 1; i <= m; i++) {
+            int x = traveler[i].x;
+            int y = traveler[i].y;
+            if(sx <= x && x < sx + squareSize && sy <= y && y < sy + squareSize) {
+                int ox = x - sx, oy = y - sy;
+                int rx = oy, ry = squareSize - ox - 1;
+                traveler[i].x = rx + sx;
+                traveler[i].y = ry + sy;
+            }
+        }
+    
+        int x = exits.x;
+        int y = exits.y;
+        if(sx <= x && x < sx + squareSize && sy <= y && y < sy + squareSize) {
+            int ox = x - sx, oy = y - sy;
+            int rx = oy, ry = squareSize - ox - 1;
+            exits.x = rx + sx;
+            exits.y = ry + sy;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                board[i][j] = sc.nextInt();
+        
+        for(int i = 1; i <= m; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            traveler[i] = new Pair(x, y);
+        }
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        exits = new Pair(x, y);
+
+        while(k-- > 0) {
+            moveAllTraveler();
+
+            boolean isAllEscaped = true;
+            for(int i = 1; i <= m; i++) {
+                if(!(traveler[i].x == exits.x && traveler[i].y == exits.y)) {
+                    isAllEscaped = false;
+                }
+            }
+
+            if(isAllEscaped) break;
+
+            findMinimumSquare();
+
+            rotateSquare();
+            rotateTravelerAndExit();
+        }
+
+        System.out.print(ans + "\n");
+        System.out.print(exits.x + " " + exits.y);
     }
 }
