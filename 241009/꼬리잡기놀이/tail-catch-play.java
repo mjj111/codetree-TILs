edcea1011c60f96cb9e5effdad93b9a40ab82b59
김명준 <86913355+mjj111@users.noreply.github.com>
2024-10-09 21:05:21 +0900

    Title: 꼬리잡기놀이, Time: 231ms, Memory: 12MB, Status: Passed - CodeTree

--- 241009/꼬리잡기놀이/tail-catch-play.java
+++ 241009/꼬리잡기놀이/tail-catch-play.java
@@ -1,331 +1,234 @@
-// 40분동안 수도코드 작성
-// 5시 17분 시작 
+import java.util.Scanner;
+import java.util.ArrayList;
 
-//n * n 격자
-//3명 이상이 한 팀
-//각 팀은 게임에서 주어진 이동 선을 따라서만 이동합니다. 
-//각 팀의 이동 선은 끝이 이어져있습니다. 각 팀의 이동 선은 서로 겹치지 않습니다.
-//게임은 라운드 별로 진행
+class Pair {
+    int x, y;
 
-//1. 머리 사람을 따라서 한 칸 이동
+    public Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
-//2. 각 라운드마다 공이 정해진 선을 따라 던져집니다.
-// 1부터 n 행 오른쪽 방향
-// n +1 부터 2n 열 윗방향   
-// 2n + 1 부터 3n 행 왼쪽 방향 
-// 3n + 1 부터 4n 열 아래방향 
-
-// 3. 공을 던졌을 때 최초 맞은 사람은 점수획득 
-// 3.1 점수는 머릿사람을 시작으로 팀내의 K 번째 사람이라면 K 제곱만큼 점수 획득
-// 3.2 획득시에 팀의 머릿사람과 꼬릿사람이 바뀐다. (방향이 바뀜)
-// 각 팀이 획득한 점수의 총합을 구하는 프로그램을 구하세요.
-
-
-// 0은 빈칸, 1은 머리사람, 2는 머리사람과 꼬리사람이 아닌 나머지, 3은 꼬리사람, 4 는  이동선
-// 1인 머리 사람들의 위치를 팀 단위로 따로 저장해놓는다. Map<팀 번호, 머리사람 위치xy 꼬리사람 위치,길이> 
-// 각 팀마다 머리 사람들 위치로 시작해서 길이를 측정하고 갱신한다. dfs로 4를 만날떄까지 길이를 측정한다. 
-// board는 만난 영역은 4로 바꿔주고 peopleBoard는 각 숫자로 갱신해준다. 
-
-// 1. 머리 사람을 따라서 한 칸 이동
-// 머리사람은 4인 경로를 찾아, 해당 위치부터 반대로 길이만큼 이동하며 peopleBoard에숫자를 매겨준다. 
-// 마지막은 0으로 매겨줘야한다.(꼬리 삭제) 
-
-// 2. 각 라운드마다 공이 정해진 선을 따라 던져집니다.
-// 해당 라운드에 적용되는 라인을 찾는다. 
-// x,y 좌표 dir(하우상좌), binDir(우상좌우)
-// 전달할꺼 따로 저장해놓고
-// dir로 이동한다.
-// 만약 벽에 부딪히면 bimDir, dir 90도 회전
-
-// 3. 공을 던졌을 때 최초 맞은 사람은 점수획득 
-// 해당bimDir 방향으로 bim을 계속 움직인다. 
-// while(isIn(x,y)) 인 동안 반복해서 bim을 움직인다.
-// 만약 만날경우! (!!!)break; 
-// !!! peopleBoard에서 만난 녀석의 값 Math.pow(값, 2) 줄 점수를 계산한다.
-// 녀석의 꼬리 혹은 머리를 탐색한다.
-// 탐색했다면 해당 팀의 머리와 꼬리를 바꾼다. 
-// 해당 팀의 점수를 추가해준다.  
-
-import java.util.*;
 public class Main {
+    public static final int DIR_NUM = 4;
+    public static final int MAX_M = 5;
+    public static final int MAX_N = 20;
+    
+    public static int n, m, k;
+    public static int[][] board = new int[MAX_N + 1][MAX_N + 1];
+    
+    // 각 팀별 레일 위치를 관리합니다.
+    public static ArrayList<Pair>[] v = new ArrayList[MAX_M + 1];
+    // 각 팀별 tail 위치를 관리합니다.
+    public static int[] tail = new int[MAX_M + 1];
+    public static boolean[][] visited = new boolean[MAX_N + 1][MAX_N + 1];
+    
+    // 격자 내 레일에 각 팀 번호를 적어줍니다.
+    public static int[][] boardIdx = new int[MAX_N + 1][MAX_N + 1];
+    
+    public static int ans;
+    
+    public static int[] dx = new int[]{-1, 0, 1, 0};
+    public static int[] dy = new int[]{0, -1, 0, 1};
+    
+    public static boolean isOutRange(int x, int y) {
+        return !(1 <= x && x <= n && 1 <= y && y <= n);
+    }
+    
+    // 초기 레일을 만들기 위해 dfs를 이용합니다.
+    public static void dfs(int x, int y, int idx) {
+        visited[x][y] = true;
+        boardIdx[x][y] = idx;
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(isOutRange(nx, ny)) continue;
+    
+            // 이미 지나간 경로거나 경로가 아니면 넘어갑니다.
+            if(board[nx][ny] == 0) continue;
+            if(visited[nx][ny]) continue;
+    
+            // 가장 처음 탐색할 때 2가 있는 방향으로 dfs를 진행합니다.
+            if(v[idx].size() == 1 && board[nx][ny] != 2) continue;
+    
+            v[idx].add(new Pair(nx, ny));
+            if(board[nx][ny] == 3) tail[idx] = v[idx].size();
+            dfs(nx, ny, idx);
+        }
+    }
+    
+    // 입력을 받는 등 초기 작업을 합니다.
+    public static void init() {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        k = sc.nextInt();
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                board[i][j] = sc.nextInt();
+    
+        for(int i = 1; i <= m; i++)
+            v[i] = new ArrayList<>();
 
-    private static int N, M, K,round;
+        int cnt = 1;
+    
+        // 레일을 벡터에 저장합니다. 머리사람을 우선 앞에 넣어줍니다.
+        for(int i = 1; i <= n; i++)
+            for(int j = 1; j <= n; j++)
+                if(board[i][j] == 1) v[cnt++].add(new Pair(i, j));
+    
+        // dfs를 통해 레일을 벡터에 순서대로 넣어줍니다.
+        for(int i = 1; i <= m; i++)
+            dfs(v[i].get(0).x, v[i].get(0).y, i);
+    }
+    
+    // 각 팀을 이동시키는 함수입니다.
+    public static void moveAll() {
+        for(int i = 1; i <= m; i++) {
+            // 각 팀에 대해 레일을 한 칸씩 뒤로 이동시킵니다.
+            Pair tmp = v[i].get(v[i].size() - 1);
+            for(int j = v[i].size() - 1; j >= 1; j--)
+                v[i].set(j, v[i].get(j - 1));
+            v[i].set(0, tmp);
+        }
+    
+        for(int i = 1; i <= m; i++) {
+            // 벡터에 저장한 정보를 바탕으로 보드의 표기 역시 바꿔줍니다.
+            for(int j = 0; j < v[i].size(); j++) {
+                Pair pos = v[i].get(j);
+                if(j == 0)
+                    board[pos.x][pos.y] = 1;
+                else if(j < tail[i] - 1)
+                    board[pos.x][pos.y] = 2;
+                else if(j == tail[i] - 1)
+                    board[pos.x][pos.y] = 3;
+                else
+                    board[pos.x][pos.y] = 4;
+            }
+        }
+    }
+    
+    // (x, y) 지점에 공이 닿았을 때의 점수를 계산합니다.
+    public static void getPoint(int x, int y) {
+        int idx = boardIdx[x][y];
+        int cnt = 0;
+        for(int i = 0; i < v[idx].size(); i++)
+            if(v[idx].get(i).x == x && v[idx].get(i).y == y) 
+                cnt = i;
 
-    private static int[][] board;    
-    private static int[][] peopleBoard;
-    private static boolean[][] visited;
+        ans += (cnt + 1) * (cnt + 1);
+    }
+    
+    // turn 번째 라운드의 공을 던집니다.
+    // 공을 던졌을 때 이를 받은 팀의 번호를 반환합니다.
+    public static int throwBall(int turn) {
+        int t = (turn - 1) % (4 * n) + 1;
+    
+        if(t <= n) {
+            // 1 ~ n 라운드의 경우 왼쪽에서 오른쪽 방향으로 공을 던집니다.
+            for(int i = 1; i <= n; i++) {
+                if(1 <= board[t][i] && board[t][i] <= 3) {
+                    // 사람이 있는 첫 번째 지점을 찾습니다.
+                    // 찾게 되면 점수를 체크한 뒤 찾은 사람의 팀 번호를 저장합니다.
+                    getPoint(t, i);
+                    return boardIdx[t][i];
+                }
+            }
+        }
+        else if(t <= 2 * n) {
+            // n+1 ~ 2n 라운드의 경우 아래에서 윗쪽 방향으로 공을 던집니다.
+            t -= n;
+            for(int i = 1; i <= n; i++) {
+                if(1 <= board[n + 1 - i][t] && board[n + 1 - i][t] <= 3) {
+                    // 사람이 있는 첫 번째 지점을 찾습니다.
+                    // 찾게 되면 점수를 체크한 뒤 찾은 사람의 팀 번호를 저장합니다.
+                    getPoint(n + 1 - i, t);
+                    return boardIdx[n + 1 - i][t];
+                }
+            }
+        }
+        else if(t <= 3 * n) {
+            // 2n+1 ~ 3n 라운드의 경우 오른쪽에서 왼쪽 방향으로 공을 던집니다.
+            t -= (2 * n);
+            for(int i = 1; i <= n; i++) {
+                if(1 <= board[n + 1 - t][n + 1 - i] && board[n + 1 - t][n + 1 - i] <= 3) {
+                    // 사람이 있는 첫 번째 지점을 찾습니다.
+                    // 찾게 되면 점수를 체크한 뒤 찾은 사람의 팀 번호를 저장합니다.
+                    getPoint(n + 1 - t, n + 1 - i);
+                    return boardIdx[n + 1 - t][n + 1 - i];
+                }
+            }
+        }
+        else {
+            // 3n+1 ~ 4n 라운드의 경우 위에서 아랫쪽 방향으로 공을 던집니다.
+            t -= (3 * n);
+            for(int i = 1; i <= n; i++) {
+                if(1 <= board[i][n + 1 - t] && board[i][n + 1 - t] <= 3) {
+                    // 사람이 있는 첫 번째 지점을 찾습니다.
+                    // 찾게 되면 점수를 체크한 뒤 찾은 사람의 팀 번호를 저장합니다.
+                    getPoint(i, n + 1 - t);
+                    return boardIdx[i][n + 1 - t];
+                }
+            }
+        }
+        // 공이 그대로 지나간다면 0을 반환합니다.
+        return 0;
+    }
+    
+    // 공을 획득한 팀을 순서를 바꿉니다.
+    public static void reverse(int gotBallIdx) {
+        // 아무도 공을 받지 못했으면 넘어갑니다.
+        if(gotBallIdx == 0) return;
+    
+        int idx = gotBallIdx;
+    
+        ArrayList<Pair> newV = new ArrayList<>();
+    
+        // 순서를 맞춰 newV에 레일을 넣어줍니다.
+        for(int i = tail[idx] - 1; i >= 0; i--) {
+            Pair pos = v[idx].get(i);
+            newV.add(pos);
+        }
+    
+        for(int i = v[idx].size() - 1; i >= tail[idx]; i--) {
+            Pair pos = v[idx].get(i);
+            newV.add(pos);
+        }
 
-    private static Map<Integer,Team> teamMap;
-
-    private static int[] dx = {1,0,-1,0};//(하우상좌)
-    private static int[] dy = {0,1,0,-1};
-
-    private static int[] line = new int[3];
-    private static Team foundTeam;
+        // newV값을 다시 v[idx]에 옮겨줍니다.
+        for(int i = 0; i < v[idx].size(); i++)
+            v[idx].set(i, newV.get(i));
+    
+        // 벡터에 저장한 정보를 바탕으로 보드의 표기 역시 바꿔줍니다.
+        for(int j = 0; j < v[idx].size(); j++) {
+            Pair pos = v[idx].get(j);
+            if(j == 0)
+                board[pos.x][pos.y] = 1;
+            else if(j < tail[idx] - 1)
+                board[pos.x][pos.y] = 2;
+            else if(j == tail[idx] - 1)
+                board[pos.x][pos.y] = 3;
+            else
+                board[pos.x][pos.y] = 4;
+        }
+    }
 
     public static void main(String[] args) {
-        line = new int[]{0,0,0};
-        Scanner sc = new Scanner(System.in);
+        // 입력을 받고 구현을 위한 기본적인 처리를 합니다.
+        init();
 
-        N = sc.nextInt(); //격자의 크기 n
-        M = sc.nextInt(); // 팀의 개수 m
-        K = sc.nextInt(); //라운드 수 k
+        for(int i = 1; i <= k; i++) {
+            // 각 팀을 머리사람을 따라 한칸씩 이동시킵니다.
+            moveAll();
 
-        // 1인 머리 사람들의 위치를 팀 단위로 따로 저장해놓는다.
-        List<int[]> heads = new ArrayList<>();
+            // i번째 라운드의 공을 던집니다. 공을 받은 사람을 찾아 점수를 추가합니다.
+            int gotBallIdx = throwBall(i);
 
-        board = new int[N][N];
-        peopleBoard = new int[N][N];
-
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                int value = sc.nextInt();
-                board[i][j] = value;
-
-                if(value == 1) {
-                    heads.add(new int[]{i,j});
-                }
-            }
+            // 공을 획득한 팀의 방향을 바꿉니다.
+            reverse(gotBallIdx);
         }
 
-        // 팀 단위로 저장한다. Map<팀 번호, Team()머리사람 위치xy 꼬리사람 위치,길이> 
-        teamMap = new HashMap<>();
-        for(int i = 0; i < heads.size(); i++) {
-            int[] head = heads.get(i);
-            teamMap.put(i, new Team(head));
-
-            findTail(i,head,1);
-        }
-
-        while(K-- > 0) {
-            peopleMove();
-            ballAttack();
-            
-        }
-
-        int answer =0;
-        for(Team t : teamMap.values()) {
-            answer += t.point;
-        }
-        System.out.println(answer);
-    }
-
-    private static void printPeople() {
-        for(int i =0; i < N; i++) {
-            System.out.println();
-            for(int j = 0; j < N; j++) {
-                System.out.print(peopleBoard[i][j] + " ");
-            }
-        }
-        System.out.println();
-    }
-
-    // 2. 각 라운드마다 공이 정해진 선을 따라 던져집니다.
-    private static void ballAttack() {
-        // 해당 라운드에 적용되는 라인을 찾는다. 
-        int x = line[0];
-        int y = line[1];
-        int vDir = (line[2] + 1) % 4;
-
-        // 3. 공을 던졌을 때 최초 맞은 사람은 점수획득 
-        while(!isOut(x,y)) {
-            int value = peopleBoard[x][y];
-            if(value != 0) {
-                //peopleBoard에서 만난 녀석의 값 Math.pow(값, 2) 줄 점수를 계산한다.
-                int plusScore =(int)Math.pow(value, 2);
-                // 녀석의 팀을 탐색한다.
-                visited = new boolean[N][N];
-                findTeam(x,y);
-                // 탐색했다면 해당 팀의 머리와 꼬리를 바꾼다. 
-                foundTeam.change();
-
-                // 해당 팀의 점수를 추가해준다.  
-                foundTeam.point += plusScore;
-                foundTeam = null;
-                break;
-            }
-            // 해당bimDir 방향으로 bim을 계속 움직인다. 
-            x += dx[vDir];
-            y += dy[vDir];
-        }
-
-        //전달할꺼 따로 저장해놓고 dir로 이동한다
-        moveLine();
-        
-    }
-
-    private static void findTeam(int x, int y) {
-        int value = peopleBoard[x][y];
-        visited[x][y] = true;
-
-        // 머리 찾음
-        if(value == 1) {
-            for(Team t : teamMap.values()) {
-                if(t.headX == x && t.headY == y) {
-                    foundTeam = t;
-                    return;
-                }
-            }
-        }
-
-        // 꼬리찾음 
-        if(value == 3) {
-            for(Team t : teamMap.values()) {
-                if(t.tailX == x && t.tailY == y) {
-                    foundTeam = t;
-                    return;
-                }
-            }
-        }
-
-
-        for(int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(isOut(nx,ny)) continue; 
-            if(visited[nx][ny]) continue;
-
-            if(peopleBoard[nx][ny] != 0) {
-                findTeam(nx, ny);
-            }
-        }
-    }
-
-    private static void moveLine() {
-        int xLine = line[0];
-        int yLine = line[1];
-        int dir = line[2];
-
-        int nx = xLine + dx[dir];
-        int ny = yLine + dy[dir];
-        // 만약 벽에 부딪히면 dir 90도 회전
-        if(isOut(nx,ny)) {
-            line[2] = (line[2] + 1) % 4;
-            nx = xLine;
-            ny = yLine;
-        }
-
-        // dir로 이동한다.
-        line[0] = nx;
-        line[1] = ny;
-    }
-
-
-    // 1. 머리 사람을 따라서 한 칸 이동
-    private static void peopleMove() {
-        for(Team t : teamMap.values()) {
-
-            int x = t.headX;
-            int y = t.headY;
-
-            for(int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i]; 
-
-                if(isOut(nx,ny))continue;
-                // 머리사람은 4인 경로를 찾아,
-                if(board[nx][ny] == 4 && peopleBoard[nx][ny] != 2) {
-                    //해당 위치부터 반대로 길이만큼 이동하며 peopleBoard에숫자를 매겨준다. 
-                    //마지막은 0으로 매겨줘야한다.(꼬리 삭제) 
-                    visited = new boolean[N][N];
-                    t.move(nx,ny,1);
-                    break;
-                }
-            }
-        }
-    }
-
-    // 각 팀마다 머리 사람들 위치로 시작해서 길이를 측정하고 갱신한다. 
-    private static void findTail(int teamNumber, int[] now, int len) {
-        int x = now[0];
-        int y = now[1];
-        int nowValue = board[x][y];
-
-        //board는 만난 영역은 4로 바꿔주고 peopleBoard는 각 숫자로 갱신해준다. 
-        board[x][y] = 4;
-        peopleBoard[x][y] = len;
-
-        //dfs로 3를 만날떄까지 길이를 측정한다. 
-        if(nowValue == 3) {
-            // 팀 단위로 저장한다. Map<팀 번호, Team()머리사람 위치xy 꼬리사람 위치,길이> 
-            Team nowTeam = teamMap.get(teamNumber);
-            nowTeam.tailX = x;
-            nowTeam.tailY = y;
-            nowTeam.len = len;
-            return;
-        }
-
-        for(int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if(isOut(nx,ny)) continue;
-            if(peopleBoard[nx][ny] != 0) continue;
-            if(board[nx][ny] == 0 || board[nx][ny] == 4) continue;
-            if(len == 1 && board[nx][ny] != 2) continue;
-
-            findTail(teamNumber, new int[]{nx,ny}, len + 1);
-            
-        }
-    }
-
-    private static boolean isOut(int x, int y) {
-        return x < 0 || x >= N || y < 0 || y >= N;
-    }
-
-    private static class Team {
-        int headX, headY, tailX, tailY, len;
-        int point = 0;
-
-        public void change() {
-            int tmpX = headX;
-            int tmpY = headY;
-            headX = tailX;
-            headY = tailY;
-
-            tailX = tmpX;
-            tailY = tmpY;
-            peopleBoard[headX][headY] = 1;
-        }
-
-        public Team(int[] point) {
-            headX = point[0];
-            headY = point[1]; 
-        }
-
-        //해당 위치부터 반대로 길이만큼 이동하며 peopleBoard에숫자를 매겨준다. 
-        public void move(int x, int y, int count) {
-            if(count > len) return;
-            if(count == 1) {
-                headX = x;
-                headY = y;
-            } 
-            if(count == len) {
-                //마지막은 0으로 매겨줘야한다.(꼬리 삭제) 
-                if(tailX != headX || tailY != headY) {
-                    peopleBoard[tailX][tailY] = 0;
-                }
-                tailX = x;
-                tailY = y;
-            }
-
-            peopleBoard[x][y] = count;
-            visited[x][y] = true;
-            for(int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i]; 
-
-                if(isOut(nx,ny)) continue; 
-                if(visited[nx][ny]) continue;
-                if(board[nx][ny] ==0 )continue;
-                if(board[nx][ny] == 4 && peopleBoard[nx][ny] >= 1) {
-                    if(count == 1) {
-                        if(peopleBoard[nx][ny] != 1) continue;
-                    }
-                    move(nx,ny, count + 1);
-                }
-            }
-        }            
+        System.out.print(ans);
     }
 }
